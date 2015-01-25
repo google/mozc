fb60620071024ba9398e1b4aab41090b555e0cbe
Yohei Yukawa <yukawa@google.com>
2015-01-25 06:42:09 +0000

    Fix test failure in release build with Visual C++ 2013
    Seems that the optimization in release mode of Visual C++ 2013 is a bit too aggressive when an API is dynamically hooked up.  This CL relaxes the test case in question a bit as a workaround.
    
    No production code is changed.  Bahavior change is not intended with this CL.
    
    git-svn-id: https://mozc.googlecode.com/svn/trunk@502 a6090854-d499-a067-5803-1114d4e51264

--- src/base/win_api_test_helper_test.cc
+++ src/base/win_api_test_helper_test.cc
@@ -41,8 +41,6 @@ DWORD WINAPI GetVersionHook() {
 }
 
 TEST(WinAPITestHelperTest, BasicTest) {
-  const auto true_version = GetVersion();
-
   vector<WinAPITestHelper::HookRequest> requests;
   requests.push_back(DEFINE_HOOK("kernel32.dll", GetVersion, GetVersionHook));
 
@@ -53,7 +51,7 @@ TEST(WinAPITestHelperTest, BasicTest) {
   WinAPITestHelper::RestoreHook(restore_info);
   restore_info = nullptr;
 
-  EXPECT_EQ(GetVersion(), true_version);
+  EXPECT_NE(GetVersion(), kFakeWindowsVersion);
 }
 
 }  // namespace
