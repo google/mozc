ab4569e73bca8d2375262d243f362c7b848646da
Yohei Yukawa <yukawa@google.com>
2016-01-02 15:06:49 -0800

    Unify the capitalization rule of copyright notice.
    This is just a style fix.
    
    BUG=
    TEST=compile
    REF_BUG=
    REF_CL=111256994

--- src/unix/emacs/mozc.el
+++ src/unix/emacs/mozc.el
@@ -1,6 +1,6 @@
 ;;; mozc.el --- minor mode to input Japanese with Mozc
 
-;; Copyright 2010-2016 Google Inc. All Rights Reserved.
+;; Copyright 2010-2016, Google Inc. All rights reserved.
 
 ;; Keywords: mule, multilingual, input method
 
