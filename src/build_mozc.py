f5ddd0e06f5d94ecc626b38797c982e8598d18e0
Yohei Yukawa <yukawa@google.com>
2018-01-01 00:00:18 -0800

    Update the copyright year to 2018
    REF_BUG=
    REF_CL=180466480
    REF_TIME=2018-01-01T00:00:18-08:00
    REF_TIME_RAW=1514793618 -0800

--- src/build_mozc.py
+++ src/build_mozc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright 2010-2017, Google Inc.
+# Copyright 2010-2018, Google Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
