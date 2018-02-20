google/mozc

commit 1aa25f461b823a43b1b92e4c4234a5bb0d0f2a80

Support minimal engine
REF_BUG=65566579
REF_CL=169654731
REF_TIME=2017-09-22T16:20:59+09:00
REF_TIME_RAW=1506064859 +0900

src/android/jni/mozcjni.cc
@@ -46,6 +46,7 @@
 #include "data_manager/data_manager.h"
 #include "engine/engine.h"
 #include "engine/engine_builder.h"
+#include "engine/minimal_engine.h"
 #include "protocol/commands.pb.h"
 #include "session/session_handler.h"
 #include "session/session_usage_observer.h"
@@ -122,24 +123,39 @@ string JstringToCcString(JNIEnv *env, jstring j_string) {
   return cc_string;
 }
 
+std::unique_ptr<DataManager> CreateDataManager(JNIEnv *env,
+                                               jstring j_data_file_path) {
+  if (j_data_file_path == nullptr) {
+    return nullptr;
+  }
+  const string &data_file_path = JstringToCcString(env, j_data_file_path);
+  std::unique_ptr<DataManager> data_manager(new TargetDataManager());
+  const auto status = data_manager->InitFromFile(data_file_path);
+  if (status != DataManager::Status::OK) {
+    LOG(INFO) << "Failed to load data file from " << data_file_path << "("
+              << DataManager::StatusCodeToString(status) << "). "
+              << "Fall back to the embedded data.";
+    data_manager.reset();
+  }
+  return data_manager;
+}
+
 std::unique_ptr<SessionHandlerInterface> CreateSessionHandler(
     JNIEnv *env, jstring data_file_path) {
   if (env == nullptr) {
     LOG(DFATAL) << "JNIEnv is null";
     return nullptr;
   }
-  std::unique_ptr<DataManager> data_manager(new TargetDataManager());
-  if (data_file_path != nullptr) {
-    const DataManager::Status status =
-        data_manager->InitFromFile(JstringToCcString(env, data_file_path));
-    if (status != DataManager::Status::OK) {
-      LOG(ERROR) << "Error in the data passed through JNI: " << status;
-      return nullptr;
-    }
+  std::unique_ptr<DataManager> data_manager =
+      CreateDataManager(env, data_file_path);
+  std::unique_ptr<EngineInterface> engine;
+  if (data_manager) {
+    engine = Engine::CreateMobileEngine(std::move(data_manager));
+  } else {
+    engine.reset(new MinimalEngine());
   }
   std::unique_ptr<SessionHandlerInterface> result(new SessionHandler(
-      Engine::CreateMobileEngine(std::move(data_manager)),
-      std::unique_ptr<EngineBuilder>(new EngineBuilder())));
+      std::move(engine), std::unique_ptr<EngineBuilder>(new EngineBuilder())));
   result->AddObserver(Singleton<session::SessionUsageObserver>::get());
   return result;
 }

src/data/version/mozc_version_template.bzl
@@ -30,7 +30,7 @@
 
 MAJOR=2
 MINOR=22
-BUILD=2730
+BUILD=2731
 REVISION=102
 # This version represents the version of Mozc IME engine (converter, predictor,
 # etc.).  This version info is included both in the Mozc server and in the Mozc

src/android/android.gyp
@@ -498,6 +498,7 @@
         '../dictionary/dictionary.gyp:dictionary',
         '../engine/engine.gyp:engine',
         '../engine/engine.gyp:engine_builder',
+        '../engine/engine.gyp:minimal_engine',
         '../session/session.gyp:session',
         '../session/session.gyp:session_handler',
         '../session/session.gyp:session_usage_observer',

commit cfe5f66700c547984bdf1f62af269bc3c7f219df

Fix a runtime error on Android OSS build
This is a follow up CL to 1aa25f461b823a43b1b92e4c4234a5bb0d0f2a80,
which broke OSS build.

Closes #432.
Closed #434.

BUG=#432, #434
TEST=manually done
REF_BUG=71956804
REF_CL=186248754
REF_TIME=2018-02-20T10:19:45+09:00
REF_TIME_RAW=1519089585 +0900

src/android/jni/mozcjni.cc
@@ -55,10 +55,15 @@
 #if !defined(MOZC_USE_CUSTOM_DATA_MANAGER)
 // Use plain DataManager, which needs to be manually initialized.
 using TargetDataManager = ::mozc::DataManager;
+constexpr bool kEmbeddedData = false;
+
+
 #else
 // Use OssDataManager, which is embedding data by default.
 #include "data_manager/oss/oss_data_manager.h"
 using TargetDataManager = ::mozc::oss::OssDataManager;
+constexpr bool kEmbeddedData = true;
+
 #endif
 
 namespace mozc {
@@ -125,6 +130,13 @@ string JstringToCcString(JNIEnv *env, jstring j_string) {
 
 std::unique_ptr<DataManager> CreateDataManager(JNIEnv *env,
                                                jstring j_data_file_path) {
+  if (kEmbeddedData) {
+    // If the data manager uses embedded data like OSS version,
+    // j_data_file_path is validly nullptr.
+    std::unique_ptr<DataManager> data_manager(new TargetDataManager());
+    return data_manager;
+  }
+
   if (j_data_file_path == nullptr) {
     return nullptr;
   }

src/data/version/mozc_version_template.bzl
@@ -30,7 +30,7 @@
 
 MAJOR=2
 MINOR=23
-BUILD=2809
+BUILD=2810
 REVISION=102
 # This version represents the version of Mozc IME engine (converter, predictor,
 # etc.).  This version info is included both in the Mozc server and in the Mozc