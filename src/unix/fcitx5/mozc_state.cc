05e6523b038a4147edb79486a3f810118e1cbcd7
Weng Xuetian <wengxt@gmail.com>
2020-12-27 11:36:09 -0800

    Fix DIRECT mode. #12

--- src/unix/fcitx5/mozc_state.cc
+++ src/unix/fcitx5/mozc_state.cc
@@ -147,8 +147,13 @@ bool MozcState::TrySendCompositionMode(mozc::commands::CompositionMode mode,
   DCHECK(out_error);
 
   mozc::commands::SessionCommand command;
-  command.set_type(mozc::commands::SessionCommand::SWITCH_INPUT_MODE);
-  command.set_composition_mode(mode);
+  if (mode == mozc::commands::DIRECT) {
+    command.set_type(mozc::commands::SessionCommand::TURN_OFF_IME);
+    command.set_composition_mode(composition_mode_);
+  } else {
+    command.set_type(mozc::commands::SessionCommand::SWITCH_INPUT_MODE);
+    command.set_composition_mode(mode);
+  }
   return TrySendRawCommand(command, out, out_error);
 }
 
