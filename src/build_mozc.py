0e5ac41427c835852797d350e0e0db4061c79186
Hiroyuki Komatsu <komatsu@google.com>
2021-01-09 15:42:34 +0900

    Update the copyright year to 2021.

--- src/build_mozc.py
+++ src/build_mozc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright 2010-2020, Google Inc.
+# Copyright 2010-2021, Google Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
--- src/win32/base/win32_base.gyp
+++ src/win32/base/win32_base.gyp
@@ -1,4 +1,4 @@
-# Copyright 2010-2020, Google Inc.
+# Copyright 2010-2021, Google Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
