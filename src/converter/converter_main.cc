b3ba4b40ec29f6b73ccb1fe9ab47e3b73c8901b9
Toshiyuki Hanaoka <toshiyuki@google.com>
2022-10-13 12:12:31 +0900

    Enable kana modifier insensitive conversion for mobile engine. The feature is enabled by default in the actual mozc in Gboard.
    PiperOrigin-RevId: 480783644

--- src/converter/converter_main.cc
+++ src/converter/converter_main.cc
@@ -453,6 +453,7 @@ int main(int argc, char **argv) {
                 absl::GetFlag(FLAGS_magic));
   CHECK_OK(data_manager);
 
+  mozc::config::Config config = mozc::config::ConfigHandler::DefaultConfig();
   mozc::commands::Request request;
   std::unique_ptr<mozc::EngineInterface> engine;
   if (absl::GetFlag(FLAGS_engine_type) == "desktop") {
@@ -461,6 +462,7 @@ int main(int argc, char **argv) {
   } else if (absl::GetFlag(FLAGS_engine_type) == "mobile") {
     engine = mozc::Engine::CreateMobileEngine(*std::move(data_manager)).value();
     mozc::commands::RequestForUnitTest::FillMobileRequest(&request);
+    config.set_use_kana_modifier_insensitive_conversion(true);
   } else {
     LOG(FATAL) << "Invalid type: --engine_type="
                << absl::GetFlag(FLAGS_engine_type);
@@ -478,8 +480,6 @@ int main(int argc, char **argv) {
   mozc::Segments segments;
   std::string line;
 
-  mozc::config::Config config;
-  mozc::config::ConfigHandler::GetDefaultConfig(&config);
   mozc::ConversionRequest conversion_request =
       mozc::ConversionRequest(nullptr, &request, &config);
   conversion_request.set_max_conversion_candidates_size(
