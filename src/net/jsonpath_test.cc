abff6615f799bf16c4d35cdc97cb0be6453fb672
Hiroshi Kurokawa <hkurokawa@google.com>
2023-02-09 03:35:55 +0900

    Swap the arguments for EXPECT_EQ/ASSERT_NE under mozc/net
    The CL makes sure that the first argument is the actual result and the second
    argument is the expected value for EXPECT_EQ.
    
    PiperOrigin-RevId: 508120198

--- src/net/jsonpath_test.cc
+++ src/net/jsonpath_test.cc
@@ -88,95 +88,95 @@ TEST_F(JsonPathTest, BasicTest) {
       "}";
 
   // Parse error
-  EXPECT_EQ("ERROR", Parse(kInput, ""));
-  EXPECT_EQ("ERROR", Parse(kInput, "$"));
-  EXPECT_EQ("ERROR", Parse(kInput, "$.foo...bar"));
-  EXPECT_EQ("ERROR", Parse(kInput, "$."));
-  EXPECT_EQ("ERROR", Parse(kInput, "."));
-  EXPECT_EQ("ERROR", Parse(kInput, "$.."));
-  EXPECT_EQ("ERROR", Parse(kInput, "$.*."));
-  EXPECT_EQ("ERROR", Parse(kInput, "$.foo]["));
-  EXPECT_EQ("ERROR", Parse(kInput, "$.foo[."));
-  EXPECT_EQ("ERROR", Parse(kInput, "$.foo[]"));
-  EXPECT_EQ("ERROR", Parse(kInput, "$.foo[10]]"));
+  EXPECT_EQ(Parse(kInput, ""), "ERROR");
+  EXPECT_EQ(Parse(kInput, "$"), "ERROR");
+  EXPECT_EQ(Parse(kInput, "$.foo...bar"), "ERROR");
+  EXPECT_EQ(Parse(kInput, "$."), "ERROR");
+  EXPECT_EQ(Parse(kInput, "."), "ERROR");
+  EXPECT_EQ(Parse(kInput, "$.."), "ERROR");
+  EXPECT_EQ(Parse(kInput, "$.*."), "ERROR");
+  EXPECT_EQ(Parse(kInput, "$.foo]["), "ERROR");
+  EXPECT_EQ(Parse(kInput, "$.foo[."), "ERROR");
+  EXPECT_EQ(Parse(kInput, "$.foo[]"), "ERROR");
+  EXPECT_EQ(Parse(kInput, "$.foo[10]]"), "ERROR");
 
   // Normal expression
-  EXPECT_EQ("hello", Parse(kInput, "$.title"));
-  EXPECT_EQ("", Parse(kInput, "$.books.title"));
-  EXPECT_EQ("", Parse(kInput, "$.books[0]"));
-  EXPECT_EQ("foo1", Parse(kInput, "$.books[0].title"));
-  EXPECT_EQ("foo1 foo2 foo3", Parse(kInput, "$.books[*].title"));
-  EXPECT_EQ("foo2", Parse(kInput, "$.books[1].title"));
-  EXPECT_EQ("foo2", Parse(kInput, "$.*[1].title"));
-  EXPECT_EQ("foo1 foo2 foo3", Parse(kInput, "$.*[*].title"));
+  EXPECT_EQ(Parse(kInput, "$.title"), "hello");
+  EXPECT_EQ(Parse(kInput, "$.books.title"), "");
+  EXPECT_EQ(Parse(kInput, "$.books[0]"), "");
+  EXPECT_EQ(Parse(kInput, "$.books[0].title"), "foo1");
+  EXPECT_EQ(Parse(kInput, "$.books[*].title"), "foo1 foo2 foo3");
+  EXPECT_EQ(Parse(kInput, "$.books[1].title"), "foo2");
+  EXPECT_EQ(Parse(kInput, "$.*[1].title"), "foo2");
+  EXPECT_EQ(Parse(kInput, "$.*[*].title"), "foo1 foo2 foo3");
   // Object names are fetched by alphabetical order.
-  EXPECT_EQ("bar1 foo1", Parse(kInput, "$.books[0].*"));
-  EXPECT_EQ("bar1 foo1 bar3 foo3", Parse(kInput, "$.books[0,2].*"));
-  EXPECT_EQ("bar3 foo3", Parse(kInput, "$.books[-1].*"));
+  EXPECT_EQ(Parse(kInput, "$.books[0].*"), "bar1 foo1");
+  EXPECT_EQ(Parse(kInput, "$.books[0,2].*"), "bar1 foo1 bar3 foo3");
+  EXPECT_EQ(Parse(kInput, "$.books[-1].*"), "bar3 foo3");
 
-  EXPECT_EQ("Foo1", Parse(kInput, "$.papers.list[0].title"));
-  EXPECT_EQ("Foo1 Foo2 Foo3", Parse(kInput, "$.papers.list[*].title"));
-  EXPECT_EQ("Foo1", Parse(kInput, "$.papers.*[0].title"));
-  EXPECT_EQ("10 11 2011", Parse(kInput, "$.papers.*"));
-  EXPECT_EQ("10", Parse(kInput, "$.papers.date"));
+  EXPECT_EQ(Parse(kInput, "$.papers.list[0].title"), "Foo1");
+  EXPECT_EQ(Parse(kInput, "$.papers.list[*].title"), "Foo1 Foo2 Foo3");
+  EXPECT_EQ(Parse(kInput, "$.papers.*[0].title"), "Foo1");
+  EXPECT_EQ(Parse(kInput, "$.papers.*"), "10 11 2011");
+  EXPECT_EQ(Parse(kInput, "$.papers.date"), "10");
 
-  EXPECT_EQ("hello foo1 foo2 foo3 Foo1 Foo2 Foo3", Parse(kInput, "$..title"));
-  EXPECT_EQ("Foo1 Foo2 Foo3", Parse(kInput, "$.papers..title"));
-  EXPECT_EQ("bar1 foo1 bar2 foo2 bar3 foo3", Parse(kInput, "$.books..*"));
+  EXPECT_EQ(Parse(kInput, "$..title"), "hello foo1 foo2 foo3 Foo1 Foo2 Foo3");
+  EXPECT_EQ(Parse(kInput, "$.papers..title"), "Foo1 Foo2 Foo3");
+  EXPECT_EQ(Parse(kInput, "$.books..*"), "bar1 foo1 bar2 foo2 bar3 foo3");
 
   // Bracket expression
-  EXPECT_EQ("hello", Parse(kInput, "$.['title']"));
-  EXPECT_EQ("", Parse(kInput, "$['books']['title']"));
-  EXPECT_EQ("", Parse(kInput, "$['books'][0]"));
-  EXPECT_EQ("foo1", Parse(kInput, "$['books'][0]['title']"));
-  EXPECT_EQ("foo1 foo2 foo3", Parse(kInput, "$['books'][*]['title']"));
-  EXPECT_EQ("foo2", Parse(kInput, "$['books'][1]['title']"));
-  EXPECT_EQ("foo2", Parse(kInput, "$[*][1]['title']"));
-  EXPECT_EQ("foo1 foo2 foo3", Parse(kInput, "$[*][*]['title']"));
+  EXPECT_EQ(Parse(kInput, "$.['title']"), "hello");
+  EXPECT_EQ(Parse(kInput, "$['books']['title']"), "");
+  EXPECT_EQ(Parse(kInput, "$['books'][0]"), "");
+  EXPECT_EQ(Parse(kInput, "$['books'][0]['title']"), "foo1");
+  EXPECT_EQ(Parse(kInput, "$['books'][*]['title']"), "foo1 foo2 foo3");
+  EXPECT_EQ(Parse(kInput, "$['books'][1]['title']"), "foo2");
+  EXPECT_EQ(Parse(kInput, "$[*][1]['title']"), "foo2");
+  EXPECT_EQ(Parse(kInput, "$[*][*]['title']"), "foo1 foo2 foo3");
   // Object names are fetched by alphabetical order.
-  EXPECT_EQ("bar1 foo1", Parse(kInput, "$['books'][0][*]"));
-  EXPECT_EQ("bar1 foo1 bar3 foo3", Parse(kInput, "$['books'][0,2][*]"));
-  EXPECT_EQ("bar3 foo3", Parse(kInput, "$['books'][-1][*]"));
+  EXPECT_EQ(Parse(kInput, "$['books'][0][*]"), "bar1 foo1");
+  EXPECT_EQ(Parse(kInput, "$['books'][0,2][*]"), "bar1 foo1 bar3 foo3");
+  EXPECT_EQ(Parse(kInput, "$['books'][-1][*]"), "bar3 foo3");
 
-  EXPECT_EQ("Foo1", Parse(kInput, "$['papers']['list'][0]['title']"));
-  EXPECT_EQ("Foo1 Foo2 Foo3", Parse(kInput, "$['papers']['list'][*]['title']"));
-  EXPECT_EQ("Foo1", Parse(kInput, "$['papers'][*][0]['title']"));
-  EXPECT_EQ("10 11 2011", Parse(kInput, "$['papers'][*]"));
-  EXPECT_EQ("10", Parse(kInput, "$['papers']['date']"));
+  EXPECT_EQ(Parse(kInput, "$['papers']['list'][0]['title']"), "Foo1");
+  EXPECT_EQ(Parse(kInput, "$['papers']['list'][*]['title']"), "Foo1 Foo2 Foo3");
+  EXPECT_EQ(Parse(kInput, "$['papers'][*][0]['title']"), "Foo1");
+  EXPECT_EQ(Parse(kInput, "$['papers'][*]"), "10 11 2011");
+  EXPECT_EQ(Parse(kInput, "$['papers']['date']"), "10");
 
   EXPECT_EQ("hello foo1 foo2 foo3 Foo1 Foo2 Foo3",
             Parse(kInput, "$..['title']"));
-  EXPECT_EQ("Foo1 Foo2 Foo3", Parse(kInput, "$['papers']..['title']"));
-  EXPECT_EQ("bar1 foo1 bar2 foo2 bar3 foo3", Parse(kInput, "$['books']..[*]"));
+  EXPECT_EQ(Parse(kInput, "$['papers']..['title']"), "Foo1 Foo2 Foo3");
+  EXPECT_EQ(Parse(kInput, "$['books']..[*]"), "bar1 foo1 bar2 foo2 bar3 foo3");
 
-  EXPECT_EQ("2011 11 10", Parse(kInput, "$['papers']['year','month','date']"));
-  EXPECT_EQ("11", Parse(kInput, "$['papers']['foo','month','bar']"));
+  EXPECT_EQ(Parse(kInput, "$['papers']['year','month','date']"), "2011 11 10");
+  EXPECT_EQ(Parse(kInput, "$['papers']['foo','month','bar']"), "11");
 }
 
 TEST_F(JsonPathTest, SliceTest) {
   constexpr char kInput[] = "{ \"a\": [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ] }";
-  EXPECT_EQ("ERROR", Parse(kInput, "$.a[]"));
-  EXPECT_EQ("0", Parse(kInput, "$.a[0]"));
-  EXPECT_EQ("3", Parse(kInput, "$.a[3]"));
-  EXPECT_EQ("", Parse(kInput, "$.a[100]"));
-  EXPECT_EQ("", Parse(kInput, "$.a[-100]"));
-  EXPECT_EQ("0", Parse(kInput, "$.a[0:1]"));
-  EXPECT_EQ("0 1", Parse(kInput, "$.a[0:2]"));
-  EXPECT_EQ("5 6", Parse(kInput, "$.a[5:7]"));
-  EXPECT_EQ("9", Parse(kInput, "$.a[-1]"));
-  EXPECT_EQ("0 1 2 3 4 5 6 7 8", Parse(kInput, "$.a[:-1]"));
-  EXPECT_EQ("9", Parse(kInput, "$.a[-1:]"));
-  EXPECT_EQ("7 8 9", Parse(kInput, "$.a[-3:]"));
-  EXPECT_EQ("6 7", Parse(kInput, "$.a[6:-2]"));
-  EXPECT_EQ("", Parse(kInput, "$.a[5:2]"));
-  EXPECT_EQ("", Parse(kInput, "$.a[-2:-3]"));
-  EXPECT_EQ("0 2", Parse(kInput, "$.a[0:4:2]"));
-  EXPECT_EQ("8 6 4", Parse(kInput, "$.a[8:2:-2]"));
-  EXPECT_EQ("", Parse(kInput, "$.a[0:4:-2]"));
-  EXPECT_EQ("4 3", Parse(kInput, "$.a[4:2:-1]"));
-  EXPECT_EQ("", Parse(kInput, "$.a[4:2:1]"));
-  EXPECT_EQ("4 5 6", Parse(kInput, "$.a[4:-3:1]"));
-  EXPECT_EQ("0 1 2 3 4 2 3 4 5 6", Parse(kInput, "$.a[0:3,3:5,2:-3]"));
+  EXPECT_EQ(Parse(kInput, "$.a[]"), "ERROR");
+  EXPECT_EQ(Parse(kInput, "$.a[0]"), "0");
+  EXPECT_EQ(Parse(kInput, "$.a[3]"), "3");
+  EXPECT_EQ(Parse(kInput, "$.a[100]"), "");
+  EXPECT_EQ(Parse(kInput, "$.a[-100]"), "");
+  EXPECT_EQ(Parse(kInput, "$.a[0:1]"), "0");
+  EXPECT_EQ(Parse(kInput, "$.a[0:2]"), "0 1");
+  EXPECT_EQ(Parse(kInput, "$.a[5:7]"), "5 6");
+  EXPECT_EQ(Parse(kInput, "$.a[-1]"), "9");
+  EXPECT_EQ(Parse(kInput, "$.a[:-1]"), "0 1 2 3 4 5 6 7 8");
+  EXPECT_EQ(Parse(kInput, "$.a[-1:]"), "9");
+  EXPECT_EQ(Parse(kInput, "$.a[-3:]"), "7 8 9");
+  EXPECT_EQ(Parse(kInput, "$.a[6:-2]"), "6 7");
+  EXPECT_EQ(Parse(kInput, "$.a[5:2]"), "");
+  EXPECT_EQ(Parse(kInput, "$.a[-2:-3]"), "");
+  EXPECT_EQ(Parse(kInput, "$.a[0:4:2]"), "0 2");
+  EXPECT_EQ(Parse(kInput, "$.a[8:2:-2]"), "8 6 4");
+  EXPECT_EQ(Parse(kInput, "$.a[0:4:-2]"), "");
+  EXPECT_EQ(Parse(kInput, "$.a[4:2:-1]"), "4 3");
+  EXPECT_EQ(Parse(kInput, "$.a[4:2:1]"), "");
+  EXPECT_EQ(Parse(kInput, "$.a[4:-3:1]"), "4 5 6");
+  EXPECT_EQ(Parse(kInput, "$.a[0:3,3:5,2:-3]"), "0 1 2 3 4 2 3 4 5 6");
 }
 
 }  // namespace net
