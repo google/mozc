a36c42a34016cf67654898eb3b4da961fc76f0e5
Emma Haruka Iwao <yuryu@google.com>
2023-03-13 09:00:11 +0900

    Add a POSIX fallback implementation for DeleteRecursively.
    PiperOrigin-RevId: 516071131

--- src/base/file/recursive.cc
+++ src/base/file/recursive.cc
@@ -56,6 +56,10 @@
 #include <TargetConditionals.h>  // for TARGET_OS_OSX
 #endif                           // __APPLE__
 
+#if defined(__ANDROID__) || (defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE)
+#include <ftw.h>
+#endif  // __ANDROID__ || TARGET_OS_IPHONE
+
 #undef StrCat
 
 namespace mozc::file {
@@ -127,7 +131,26 @@ absl::Status DeleteRecursively(const absl::string_view path) {
   return absl::OkStatus();
 }
 
-#elif defined(__linux__) || (defined(TARGET_OS_OSX) && TARGET_OS_OSX)  // _WIN32
+#else  // _WIN32
+
+namespace {
+void RemoveDirectoryOrLog(const char *path) {
+  const absl::Status s = FileUtil::RemoveDirectory(path);
+  if (!s.ok() && !absl::IsNotFound(s)) {
+    LOG(ERROR) << "Cannot remove directory " << path << ":" << s;
+  }
+}
+
+void UnlinkFileOrLog(const char *path) {
+  const absl::Status s = FileUtil::Unlink(path);
+  if (!s.ok() && !absl::IsNotFound(s)) {
+    LOG(ERROR) << "Cannot unlink " << path << ":" << s;
+  }
+}
+}  // namespace
+
+#if (defined(__linux__) && !defined(__ANDROID__)) || \
+    (defined(TARGET_OS_OSX) && TARGET_OS_OSX)
 
 absl::Status DeleteRecursively(const absl::string_view path) {
   // fts is not POSIX, but it's available on both Linux and MacOS.
@@ -149,13 +172,9 @@ absl::Status DeleteRecursively(const absl::string_view path) {
   };
 
   while (FTSENT *ent = fts_read(ftsp)) {
-    absl::Status s;
     switch (ent->fts_info) {
       case FTS_DP:  // directory postorder
-        s = FileUtil::RemoveDirectory(ent->fts_path);
-        if (!s.ok() && !absl::IsNotFound(s)) {
-          LOG(ERROR) << "Cannot remove directory " << ent->fts_path << ":" << s;
-        }
+        RemoveDirectoryOrLog(ent->fts_path);
         break;
       case FTS_F:  // regular file
         [[fallthrough]];
@@ -166,10 +185,7 @@ absl::Status DeleteRecursively(const absl::string_view path) {
       case FTS_SLNONE:  // broken symlink
         [[fallthrough]];
       case FTS_DEFAULT:  // others
-        s = FileUtil::Unlink(ent->fts_path);
-        if (!s.ok() && !absl::IsNotFound(s)) {
-          LOG(ERROR) << "Cannot unlink " << ent->fts_path << ":" << s;
-        }
+        UnlinkFileOrLog(ent->fts_path);
         break;
       default:
         break;
@@ -181,10 +197,46 @@ absl::Status DeleteRecursively(const absl::string_view path) {
   return absl::OkStatus();
 }
 
-#else  // __linux__ || TARGET_OS_OSX
+#else  // (__linux__ && !__ANDROID__) || TARGET_OS_OSX
 
-#error "Recursive is not available on this platform."
+namespace {
+int FtwDelete(const char *path, const struct stat *sb, int typeflag,
+              struct FTW *ftwbuf) {
+  switch (typeflag) {
+    case FTW_DP:  // directory postorder
+      [[fallthrough]];
+    case FTW_DNR:  // directory which can't be read. will try anyways.
+      RemoveDirectoryOrLog(path);
+      break;
+    case FTW_F:  // regular file
+      [[fallthrough]];
+    case FTW_NS:  // stat failure (no execute permission)
+      [[fallthrough]];
+    case FTW_SL:  // symlink
+      [[fallthrough]];
+    case FTW_SLN:  // broken symlink
+      UnlinkFileOrLog(path);
+      break;
+    default:
+      break;
+  }
+  return 0;
+}
+}  // namespace
 
-#endif  // !(_WIN32 || __linux__ || TARGET_OS_OSX)
+absl::Status DeleteRecursively(const absl::string_view path) {
+  const std::string path_str(path);  // for null-termination.
+  // ntfw() is a POSIX interface but it's not recommended on (at least) macOS.
+  // This is a fallback implementation.
+  constexpr int kOpenFdLimit = 100;
+  if (nftw(path_str.c_str(), FtwDelete, kOpenFdLimit,
+           FTW_DEPTH | FTW_PHYS | FTW_MOUNT) < 0) {
+    return absl::ErrnoToStatus(errno, "nftw failed");
+  }
+  return absl::OkStatus();
+}
+
+#endif  // !(__linux__ && !__ANDROID__) || TARGET_OS_OSX)
+#endif  // !_WIN32
 
 }  // namespace mozc::file
