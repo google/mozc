bf965814ce30d67bb85cb76fa677261781c98791
Toshiyuki Hanaoka <toshiyuki@google.com>
2023-04-04 07:01:22 +0000

    Add a feature to cancel suffix penalty for content node.
    PiperOrigin-RevId: 521675951

--- src/session/session_handler_scenario_test.cc
+++ src/session/session_handler_scenario_test.cc
@@ -474,6 +474,12 @@ INSTANTIATE_TEST_SUITE_P(
               request.mutable_decoder_experiment_params()
                   ->set_use_typing_correction_diff_cost(true);
               return request;
+            }(),
+            []() {
+              auto request = GetMobileRequest();
+              request.mutable_decoder_experiment_params()
+                  ->set_cancel_content_word_suffix_penalty(true);
+              return request;
             }())));
 
 TEST_P(SessionHandlerScenarioTestForRequest, TestImplBase) {
