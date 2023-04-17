e28e8365d3ec1fb9a8c4c2a260e3bb296a77e798
Emma Haruka Iwao <yuryu@google.com>
2023-04-17 18:16:00 +0000

    Rename PlatformStringView to zpfstring_view, add pfchar and pfstring
    This CL split platform_string.h to pfchar.h and zstring_view.h, and renames the types:
    - PlatformStringView -> zpfstring_view
    - PlatformString -> pfstring
    - Add pfstring_view (alias to string_view or wstring_view)
    
    The C++ style allows STL-like naming for STL-like types.
    
    PiperOrigin-RevId: 524898441

--- src/base/strings/pfchar_test.cc
+++ src/base/strings/pfchar_test.cc
@@ -27,18 +27,14 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "base/strings/platform_string.h"
+#include "base/strings/pfchar.h"
 
-#include <sstream>
 #include <string>
 #include <utility>
 
-#include "base/logging.h"
 #include "testing/gmock.h"
 #include "testing/gunit.h"
-#include "absl/container/btree_set.h"
-#include "absl/container/flat_hash_set.h"
-#include "absl/hash/hash_testing.h"
+#include "absl/strings/string_view.h"
 
 #ifdef _WIN32
 #include <windows.h>
@@ -53,8 +49,8 @@ using testing::IsEmpty;
 using testing::Not;
 
 // Testing with an actual use case.
-PlatformString GetHostName() {
-  PlatformString result;
+pfstring GetHostName() {
+  pfstring result;
 #ifdef _WIN32
   DWORD size = 0;
   EXPECT_FALSE(GetComputerNameExW(ComputerNameDnsHostname, nullptr, &size));
@@ -71,73 +67,23 @@ PlatformString GetHostName() {
   return result;
 }
 
-TEST(PlatformStringTest, GetHostName) {
-  const PlatformString s = GetHostName();
-  EXPECT_THAT(ToString(s), Not(IsEmpty()));
+TEST(PFCharTest, GetHostName) {
+  const pfstring s = GetHostName();
+  EXPECT_THAT(to_string(s), Not(IsEmpty()));
 }
 
-TEST(PlatformStringTest, ToPlatformString) {
-  constexpr PlatformStringView expected = PLATFORM_STRING("test string");
-  const std::string s = "test string";
-  EXPECT_EQ(ToPlatformString(s), expected);
-  EXPECT_EQ(ToPlatformString(std::move(s)), expected);
-}
-
-TEST(PlatformStringTest, ToString) {
+TEST(PFCharTest, ToString) {
   constexpr absl::string_view expected = "test string";
-  const PlatformString ps = PLATFORM_STRING("test string");
-  EXPECT_EQ(ToString(ps), expected);
-  EXPECT_EQ(ToString(std::move(ps)), expected);
+  const pfstring ps = PF_STRING("test string");
+  EXPECT_EQ(to_string(ps), expected);
+  EXPECT_EQ(to_string(std::move(ps)), expected);
 }
 
-TEST(PlatformStringViewTest, Nullptr) {
-  constexpr PlatformStringView psv;
-  EXPECT_THAT(psv, IsEmpty());
-  EXPECT_EQ(psv.c_str(), nullptr);
-  EXPECT_TRUE(psv->empty());
-}
-
-TEST(PlatformStringViewTest, Smoke) {
-  constexpr PlatformStringView psv = PLATFORM_STRING("test string");
-
-  EXPECT_EQ(psv, PlatformString(PLATFORM_STRING("test string")));
-  EXPECT_EQ(psv, PLATFORM_STRING("test string"));
-  EXPECT_EQ(PLATFORM_STRING("test string"), psv);
-  EXPECT_NE(psv, PLATFORM_STRING(""));
-  EXPECT_NE(PLATFORM_STRING(""), psv);
-  EXPECT_LT(psv, PLATFORM_STRING("z"));
-  EXPECT_LT(PLATFORM_STRING("a"), psv);
-  const PlatformStringView::element_type sv = *psv;
-  EXPECT_EQ(sv, psv);
-  const std::string s = ToString(psv);
-  EXPECT_EQ(s, "test string");
-
-  std::basic_ostringstream<PlatformChar> os;
-  os << psv;
-  EXPECT_EQ(os.str(), sv);
-}
-
-TEST(PlatformStringViewTest, Container) {
-  const PlatformString s1 = PLATFORM_STRING("test1");
-  const PlatformString s2 = PLATFORM_STRING("test2");
-  absl::flat_hash_set<PlatformStringView> set;
-  set.insert(s1);
-  set.insert(s2);
-  EXPECT_TRUE(set.contains(s1));
-
-  absl::btree_set<PlatformStringView> btree_set;
-  btree_set.insert(s1);
-  btree_set.insert(s2);
-  EXPECT_TRUE(btree_set.contains(s1));
-}
-
-TEST(PlatformStringViewTest, AbslHash) {
-  EXPECT_TRUE(absl::VerifyTypeImplementsAbslHashCorrectly({
-      PlatformStringView(),
-      PlatformStringView(PLATFORM_STRING("")),
-      PlatformStringView(PLATFORM_STRING("test")),
-      PlatformStringView(PLATFORM_STRING("私の名前は中野です。")),
-  }));
+TEST(PFCharTest, ToPFString) {
+  constexpr pfstring_view expected = PF_STRING("test string");
+  const std::string s = "test string";
+  EXPECT_EQ(to_pfstring(s), expected);
+  EXPECT_EQ(to_pfstring(std::move(s)), expected);
 }
 
 }  // namespace
