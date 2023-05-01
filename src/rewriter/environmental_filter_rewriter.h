bbb07fdb54f96ead58a4fa6a633716fd9288d0f2
Yohei Yukawa <yukawa@google.com>
2023-05-01 17:58:56 +0000

    Fix assertion errors in `std::set_intersection`
    This follow up to cdf75079b6ffb0a6fe0e3e1f8f6627d541580d83 (cl/527712115), which did several cleanups including replacing  `std::set<char32_t>` with `absl::flat_hash_set<char32_t>` in `environmental_filter_rewriter.cc`.
    
    It turns out that `CharacterGroupFinder::Initialize()` does assume `CharacterGroupFinder::multiple_codepoints_intersection_` to be a sorted container. Thus switch to `absl::btree_set<char32_t>`. Otherwise, Windows builds with debug CRT fail at `std::set_intersection` with an assertion error.
    
    PiperOrigin-RevId: 528517460

--- src/rewriter/environmental_filter_rewriter.h
+++ src/rewriter/environmental_filter_rewriter.h
@@ -61,7 +61,7 @@
 #include "data_manager/data_manager_interface.h"
 #include "request/conversion_request.h"
 #include "rewriter/rewriter_interface.h"
-#include "absl/container/flat_hash_set.h"
+#include "absl/container/btree_set.h"
 
 namespace mozc {
 
@@ -91,7 +91,7 @@ class CharacterGroupFinder {
   size_t max_length_ = 0;
   // Intersection of multiple_codepoints_. For example, for emoji, it is very
   // likely to have ZWJ (U+200D) in common.
-  absl::flat_hash_set<char32_t> multiple_codepoints_intersection_;
+  absl::btree_set<char32_t> multiple_codepoints_intersection_;
 };
 
 class EnvironmentalFilterRewriter : public RewriterInterface {
