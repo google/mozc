8233b4067f5df89026e13ff771ebbc2fa59fb95f
Yohei Yukawa <yukawa@google.com>
2023-05-29 04:58:18 +0000

    Support local cache in `updade_deps.py`
    Currently `updade_deps.py` doesn't have any local cache mechanism, meaning that it downloads required files every time it is triggered. This is not only inefficient but also problematic because it can trigger rate limit if a developer (or CI) triggers `updade_deps.py` multiple times in a short period of time.
    
    With this CL `updade_deps.py` starts using `src/third_party_cache/` as a cache directory.
    
    The first run not only downloads archive files but also store downloaded files into `src/third_party_cache/`.  Any further run simply use cache files under `src/third_party_cache/` instead of downloading the same files again.
    
    There is no change in the actual build instructions.  This is just an internal optimization behind the scene.
    
    This is an important step towards enabling GitHub cache action (#749).
    
    PiperOrigin-RevId: 536096666

--- src/build_tools/update_deps.py
+++ src/build_tools/update_deps.py
@@ -36,111 +36,308 @@ build for OSS Mozc.
 
 import argparse
 from collections.abc import Iterator
-import io
+import dataclasses
+import hashlib
 import os
 import pathlib
 import shutil
 import subprocess
 import sys
 import tarfile
+import time
 import zipfile
 
 import requests
+
 
 ABS_SCRIPT_PATH = pathlib.Path(__file__).absolute()
 # src/build_tools/fetch_deps.py -> src/
 ABS_MOZC_SRC_DIR = ABS_SCRIPT_PATH.parents[1]
 ABS_THIRD_PARTY_DIR = ABS_MOZC_SRC_DIR.joinpath('third_party')
-
-QT_ARCHIVE_URL = 'https://download.qt.io/archive/qt/5.15/5.15.9/submodules/qtbase-everywhere-opensource-src-5.15.9.tar.xz'
-JOM_ARCHIVE_URL = 'https://download.qt.io/official_releases/jom/jom_1_1_3.zip'
-WIX_ARCHIVE_URL = 'https://wixtoolset.org/downloads/v3.14.0.6526/wix314-binaries.zip'
+CACHE_DIR = ABS_MOZC_SRC_DIR.joinpath('third_party_cache')
+TIMEOUT = 600
 
 
-def IsWindows() -> bool:
-  """Returns true if the platform is Windows."""
-  return os.name == 'nt'
+@dataclasses.dataclass
+class ArchiveInfo:
+  """Third party archive file to be used to build Mozc binaries.
+
+  Attributes:
+    dest: Destination directory name under the third_party directory.
+    url: URL of the archive.
+    size: File size of the archive.
+    sha256: SHA-256 of the archive.
+  """
+  dest: str
+  url: str
+  size: int
+  sha256: str
+
+  @property
+  def filename(self) -> str:
+    """The filename of the archive."""
+    return self.url.split('/')[-1]
+
+  def __hash__(self):
+    return hash(self.sha256)
 
 
-def IsMac() -> bool:
-  """Returns true if the platform is Mac."""
-  return os.name == 'posix' and os.uname()[0] == 'Darwin'
+QT = ArchiveInfo(
+    dest='qt',
+    url='https://download.qt.io/archive/qt/5.15/5.15.9/submodules/qtbase-everywhere-opensource-src-5.15.9.tar.xz',
+    size=50389220,
+    sha256='1947deb9d98aaf46bf47e6659b3e1444ce6616974470523756c082041d396d1e',
+)
+
+JOM = ArchiveInfo(
+    dest='qt',
+    url='https://download.qt.io/official_releases/jom/jom_1_1_3.zip',
+    size=1213852,
+    sha256='128fdd846fe24f8594eed37d1d8929a0ea78df563537c0c1b1861a635013fff8',
+)
+
+WIX = ArchiveInfo(
+    dest='wix',
+    url='https://wixtoolset.org/downloads/v3.14.0.6526/wix314-binaries.zip',
+    size=41223699,
+    sha256='4c89898df3bcab13e12f7ca54399c35ad273475ad2cb6284611d00ae2d063c2c',
+)
 
 
-def DownloadQt(third_party_dir: pathlib.Path, dryrun: bool = False) -> None:
-  """Download Qt archive file and extract it.
+def get_sha256(path: pathlib.Path) -> str:
+  """Returns SHA-256 hash digest of the specified file.
 
   Args:
-    third_party_dir: third_party dir under which qt dir will be placed.
-    dryrun: true to perform dryrun.
+    path: Local path the file to calculate SHA-256 about.
+  Returns:
+    SHA-256 hash digestd of the specified file.
   """
-  qt_dir = third_party_dir.joinpath('qt').absolute()
-  if qt_dir.exists():
-    if dryrun:
-      print(f"dryrun: shutil.rmtree(r'{qt_dir}')")
-    else:
-      shutil.rmtree(qt_dir)
-  isatty = sys.stdout.isatty()
-  colmuns = os.get_terminal_size().columns if isatty else None
+  with open(path, 'rb') as f:
+    try:
+      # hashlib.file_digest is available in Python 3.11+
+      return hashlib.file_digest(f, 'sha256').hexdigest()
+    except AttributeError:
+      # Fallback to f.read().
+      h = hashlib.sha256()
+      h.update(f.read())
+      return h.hexdigest()
 
-  def QtExtractFilter(
-      members: Iterator[tarfile.TarInfo],
-  ) -> Iterator[tarfile.TarInfo]:
+
+def download(archive: ArchiveInfo, dryrun: bool = False) -> None:
+  """Download the specified file.
+
+  Args:
+    archive: ArchiveInfo to be downloaded.
+    dryrun: True if this is a dry-run.
+
+  Raises:
+    RuntimeError: When the downloaded file looks to be corrupted.
+  """
+
+  path = CACHE_DIR.joinpath(archive.filename)
+  if path.exists():
+    if (
+        path.stat().st_size == archive.size
+        and get_sha256(path) == archive.sha256
+    ):
+      # Cache hit.
+      return
+    else:
+      if dryrun:
+        print(f'dryrun: Verification failed. removing {path}')
+      else:
+        path.unlink()
+
+  if dryrun:
+    print(f'Download {archive.url} to {path}')
+    return
+
+  CACHE_DIR.mkdir(parents=True, exist_ok=True)
+  saved = 0
+  hasher = hashlib.sha256()
+  with requests.get(archive.url, stream=True, timeout=TIMEOUT) as r:
+    with ProgressPrinter() as printer:
+      with open(path, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=8192):
+          f.write(chunk)
+          hasher.update(chunk)
+          saved += len(chunk)
+          printer.print_line(f'{archive.filename}: {saved}/{archive.size}')
+  if saved != archive.size:
+    raise RuntimeError(
+        f'{archive.filename} size mismatch.'
+        f' expected={archive.size} actual={saved}'
+    )
+  actual_sha256 = hasher.hexdigest()
+  if actual_sha256 != archive.sha256:
+    raise RuntimeError(
+        f'{archive.filename} sha256 mismatch.'
+        f' expected={archive.sha256} actual={actual_sha256}'
+    )
+
+
+class ProgressPrinter:
+  """A utility to print progress message with carriage return and trancatoin."""
+
+  def __enter__(self):
+    if not sys.stdout.isatty():
+
+      class NoOpImpl:
+        """A no-op implementation in case stdout is not attached to concole."""
+
+        def print_line(self, msg: str) -> None:
+          """No-op implementation.
+
+          Args:
+            msg: Unused.
+          """
+          del msg  # Unused
+          return
+
+      self.cleaner = None
+      return NoOpImpl()
+
+    class Impl:
+      """A real implementation in case stdout is attached to concole."""
+      last_output_time_ns = time.time_ns()
+
+      def print_line(self, msg: str) -> None:
+        """Print the given message with carriage return and trancatoin.
+
+        Args:
+          msg: Message to be printed.
+        """
+        colmuns = os.get_terminal_size().columns
+        now = time.time_ns()
+        if (now - self.last_output_time_ns) < 25000000:
+          return
+        msg = msg + ' ' * max(colmuns - len(msg), 0)
+        msg = msg[0 : (colmuns)] + '\r'
+        sys.stdout.write(msg)
+        sys.stdout.flush()
+        self.last_output_time_ns = now
+
+    class Cleaner:
+      def cleanup(self) -> None:
+        colmuns = os.get_terminal_size().columns
+        sys.stdout.write(' ' * colmuns + '\r')
+        sys.stdout.flush()
+
+    self.cleaner = Cleaner()
+    return Impl()
+
+  def __exit__(self, *exc):
+    if self.cleaner:
+      self.cleaner.cleanup()
+
+
+def qt_extract_filter(
+    members: Iterator[tarfile.TarInfo],
+) -> Iterator[tarfile.TarInfo]:
+  """Custom extract filter for the Qt Tar file.
+
+  This custom filter can be used to adjust directory structure and drop
+  unnecessary files/directories to save disk space.
+
+  Args:
+    members: an iterator of TarInfo from the Tar file.
+
+  Yields:
+    An iterator of TarInfo to be extracted.
+  """
+  with ProgressPrinter() as printer:
     for info in members:
       paths = info.name.split('/')
       if '..' in paths:
         continue
       if len(paths) < 1:
         continue
-      if len(paths) >= 2 and paths[1] in ['examples']:
-        continue
-      info.name = '/'.join(paths[1:])
-      if isatty:
-        msg = 'extracting ' + info.name
-        msg = msg + ' ' * max(colmuns - len(msg), 0)
-        msg = msg[0:(colmuns)] + '\r'
-        sys.stdout.write(msg)
-        sys.stdout.flush()
-      if dryrun:
+      paths = paths[1:]
+      new_path = '/'.join(paths)
+      if len(paths) >= 1 and paths[0] == 'examples':
+        printer.print_line('skipping   ' + new_path)
         continue
       else:
+        printer.print_line('extracting ' + new_path)
+        info.name = new_path
         yield info
-  print('Downloading', QT_ARCHIVE_URL)
-  with requests.get(QT_ARCHIVE_URL, stream=True) as r:
-    with tarfile.open(fileobj=r.raw, mode='r|xz') as f:
-      f.extractall(path=qt_dir, members=QtExtractFilter(f))
-
-  # For Windows, also check out jom.exe
-  if IsWindows():
-    print('Downloading', JOM_ARCHIVE_URL)
-    with requests.get(JOM_ARCHIVE_URL) as r:
-      with zipfile.ZipFile(io.BytesIO(r.content)) as f:
-        if not dryrun:
-          f.extract('jom.exe', path=qt_dir)
 
 
-def DownloadWiX(third_party_dir: pathlib.Path, dryrun: bool = False) -> None:
-  """Download WiX archive file and extract it.
+def wix_extract_filter(
+    members: Iterator[zipfile.ZipInfo],
+) -> Iterator[zipfile.ZipInfo]:
+  """Custom extract filter for the WiX Zip archive.
+
+  This custom filter can be used to adjust directory structure and drop
+  unnecessary files/directories to save disk space.
 
   Args:
-    third_party_dir: third_party dir under which wix dir will be placed.
-    dryrun: true to perform dryrun.
+    members: an iterator of ZipInfo from the Zip archive.
+
+  Yields:
+    an iterator of ZipInfo to be extracted.
   """
-  wix_dir = third_party_dir.joinpath('wix').absolute()
-  if wix_dir.exists():
+  with ProgressPrinter() as printer:
+    for info in members:
+      paths = info.filename.split('/')
+      if '..' in paths:
+        continue
+      if len(paths) >= 2:
+        printer.print_line('skipping   ' + info.filename)
+        continue
+      else:
+        printer.print_line('extracting ' + info.filename)
+        yield info
+
+
+def extract(
+    archive: ArchiveInfo,
+    dryrun: bool = False,
+) -> None:
+  """Extract the given archive.
+
+  Args:
+    archive: ArchiveInfo to be exptracted.
+    dryrun: True if this is a dry-run.
+  """
+  dest = ABS_THIRD_PARTY_DIR.joinpath(archive.dest).absolute()
+  src = CACHE_DIR.joinpath(archive.filename)
+  if src.suffix == '.xz':
     if dryrun:
-      print(f"dryrun: shutil.rmtree(r'{wix_dir}')")
+      print(f'dryrun: Extracting {src}')
     else:
-      shutil.rmtree(wix_dir)
+      with tarfile.open(src, mode='r|xz') as f:
+        if archive == QT:
+          f.extractall(path=dest, members=qt_extract_filter(f))
+        else:
+          f.extractall(path=dest)
+  elif src.suffix == '.zip':
+    if dryrun:
+      print(f'dryrun: Extracting {src}')
+    else:
+      def filename(members: Iterator[zipfile.ZipInfo]):
+        for info in members:
+          yield info.filename
+      with zipfile.ZipFile(src) as z:
+        if archive == WIX:
+          z.extractall(
+              path=dest, members=filename(wix_extract_filter(z.infolist()))
+          )
+        else:
+          z.extractall(path=dest)
 
-  print('Downloading', WIX_ARCHIVE_URL)
-  with requests.get(WIX_ARCHIVE_URL) as r:
-    with zipfile.ZipFile(io.BytesIO(r.content)) as f:
-      if not dryrun:
-        f.extractall(path=wix_dir)
+
+def is_windows() -> bool:
+  """Returns true if the platform is Windows."""
+  return os.name == 'nt'
 
 
-def RunUpdateSubmodules(dryrun: bool = False) -> None:
+def is_mac() -> bool:
+  """Returns true if the platform is Mac."""
+  return os.name == 'posix' and os.uname()[0] == 'Darwin'
+
+
+def update_submodules(dryrun: bool = False) -> None:
   """Run 'git submodule update --init --recursive'.
 
   Args:
@@ -150,7 +347,6 @@ def RunUpdateSubmodules(dryrun: bool = False) -> None:
   if dryrun:
     print(f'dryrun: subprocess.run({command}, shell=True, check=True)')
   else:
-    print(f'Running {command}')
     subprocess.run(command, shell=True, check=True)
 
 
@@ -160,16 +356,40 @@ def main():
   parser.add_argument('--noqt', action='store_true', default=False)
   parser.add_argument('--nowix', action='store_true', default=False)
   parser.add_argument('--nosubmodules', action='store_true', default=False)
-  parser.add_argument('--third_party_dir', help='third_party directory',
-                      type=str, default=ABS_THIRD_PARTY_DIR)
+  parser.add_argument('--cache_only', action='store_true', default=False)
+
   args = parser.parse_args()
-  third_party_dir = pathlib.Path(args.third_party_dir)
-  if (not args.noqt) and (IsWindows() or IsMac()):
-    DownloadQt(third_party_dir, args.dryrun)
-  if (not args.nowix) and IsWindows():
-    DownloadWiX(third_party_dir, args.dryrun)
+
+  archives = []
+  if (not args.noqt) and (is_windows() or is_mac()):
+    archives.append(QT)
+    if is_windows():
+      archives.append(JOM)
+  if (not args.nowix) and is_windows():
+    archives.append(WIX)
+
+  for archive in archives:
+    download(archive, args.dryrun)
+
+  if args.cache_only:
+    return
+
+  dest_dirs = set()
+  for archive in archives:
+    dest_dirs.add(ABS_THIRD_PARTY_DIR.joinpath(archive.dest))
+
+  for dest_dir in dest_dirs:
+    if dest_dir.exists():
+      if args.dryrun:
+        print(f"dryrun: shutil.rmtree(r'{dest_dir}')")
+      else:
+        shutil.rmtree(dest_dir)
+
+  for archive in archives:
+    extract(archive, args.dryrun)
+
   if not args.nosubmodules:
-    RunUpdateSubmodules(args.dryrun)
+    update_submodules(args.dryrun)
 
 
 if __name__ == '__main__':
