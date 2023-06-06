d56492af6c07dc3bd4bca386aed5216f50f1b77a
Yohei Yukawa <yukawa@google.com>
2023-06-06 14:09:30 +0000

    Stop setting `/ALLOWBIND:NO` and `/RELEASE` bits with `editbin.exe`
    As we no longer call `bind.exe`, it is no longer necessary for us to call `editbin.exe` with `/ALLOWBIND:NO` and `/RELEASE`.
    
    There must be no user observable behavior change even without such post process.
    
    See https://learn.microsoft.com/en-us/cpp/build/reference/editbin-options about `editbin.exe` options.
    
    #codehealth
    
    PiperOrigin-RevId: 538171965

--- src/win32/installer/postbuilds_win.py
+++ src/win32/installer/postbuilds_win.py
@@ -60,22 +60,13 @@ def PostProcessOnWindows(opts):
   """Apply post-processes for Windows binaries.
 
   Update the specified executable to be 'release quality' by
-  - Set 'freeze' bit in the PE header.
   - Code signing.
-  See the following issues for details.
-    http://b/1504561, http://b/1507272, http://b/2289857, http://b/1893852
 
   Args:
     opts: build options to be used to update the executable.
   """
   abs_target_path = os.path.abspath(opts.targetpath)
   abs_touch_file_path = abs_target_path + '.postbuild'
-
-  # If the target looks like a PE image, update it.
-  (_, extension) = os.path.splitext(opts.targetpath)
-  if extension.lower() in ['.exe', '.dll']:
-    # Protect it against further binding, which invalidates code signing.
-    RunOrDie(['editbin.exe', '/ALLOWBIND:NO', '/RELEASE', abs_target_path])
 
   Codesign(abs_target_path)
 
