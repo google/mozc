509ba9b8782147ee0f6f2f87c0e96bd53f48b7d1
Emma Haruka Iwao <yuryu@google.com>
2023-06-20 18:28:11 +0000

    Replace optparse with argparse, produce C++ source with constexpr
    Use argparse to parse command line arguments
    Generated code uses constexpr instead of const
    
    PiperOrigin-RevId: 541994999

--- src/base/gen_character_set.py
+++ src/base/gen_character_set.py
@@ -28,11 +28,9 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__author__ = "taku"
-
-import codecs
+"""Generates character_set.inc file."""
+import argparse
 import itertools
-import optparse
 import re
 import sys
 
@@ -57,7 +55,7 @@ def IsValidUCS4(n):
   return 0 <= n <= 0x7FFFFFFF
 
 
-class CodePointCategorizer(object):
+class CodePointCategorizer:
   """Categorizer of ucs4 code points."""
 
   _UCS2_PATTERN = re.compile(r'^0x([0-9A-F]{4})')
@@ -68,9 +66,9 @@ class CodePointCategorizer(object):
   _UCS4_PATTERN = re.compile(r'^U\+([0-9A-F]+)')
 
   def __init__(self, jisx0201file, jisx0208file):
-    self._jisx0201 = CodePointCategorizer._LoadJISX0201(jisx0201file)
-    self._jisx0208 = CodePointCategorizer._LoadJISX0208(jisx0208file)
-    self._exceptions = CodePointCategorizer._LoadExceptions()
+    self._jisx0201 = self._LoadJISX0201(jisx0201file)
+    self._jisx0208 = self._LoadJISX0208(jisx0208file)
+    self._exceptions = self._LoadExceptions()
 
     # Make a list of code point tables in the priority order.
     self._table_list = [
@@ -79,10 +77,10 @@ class CodePointCategorizer(object):
         ('JISX0208', self._jisx0208),
     ]
 
-  @staticmethod
-  def _LoadTable(filename, column_index, pattern, validater):
+  def _LoadTable(self, file, column_index, pattern, validater):
+    """Loads specified table from file."""
     result = set()
-    for line in codecs.open(filename, encoding='utf-8'):
+    for line in file:
       if line.startswith('#'):
         # Skip a comment line.
         continue
@@ -96,27 +94,26 @@ class CodePointCategorizer(object):
 
     return result
 
-  @staticmethod
-  def _LoadJISX0201(filename):
-    return CodePointCategorizer._LoadTable(
-        filename, 1, CodePointCategorizer._UCS2_PATTERN, IsValidUCS2)
+  def _LoadJISX0201(self, file):
+    return self._LoadTable(
+        file, 1, CodePointCategorizer._UCS2_PATTERN, IsValidUCS2
+    )
 
-  @staticmethod
-  def _LoadJISX0208(filename):
-    result = CodePointCategorizer._LoadTable(
-        filename, 2, CodePointCategorizer._UCS2_PATTERN, IsValidUCS2)
+  def _LoadJISX0208(self, file):
+    result = self._LoadTable(
+        file, 2, CodePointCategorizer._UCS2_PATTERN, IsValidUCS2
+    )
     result.update([
         0xFF3C,  # (FULLWIDTH REVERSE SOLIDS) should be in JISX0208
         0xFF0D,  # (FULLWIDTH HYPHEN MINUS) should be in JISX0208
-        ])
+    ])
     return result
 
   # The following chars have different mapping in
   # Windows and Mac. Technically, they are platform dependent
   # characters, but Mozc treat them so that they are normal characters
   # defined in JISX0208
-  @staticmethod
-  def _LoadExceptions():
+  def _LoadExceptions(self):
     # treat Unicode Japanese incompatible characters as JISX0208.
     return set([
         0x00A5,  # YEN SIGN
@@ -133,7 +130,7 @@ class CodePointCategorizer(object):
         0xFFE1,  # FULL WIDTH POUND SIGN
         0x00AC,  # NOT SIGN
         0xFFE2,  # FULL WIDTH NOT SIGN
-        ])
+    ])
 
   def GetCategory(self, codepoint):
     """Returns category name of the codepoint, or None for invalid input."""
@@ -176,27 +173,36 @@ def GroupConsecutiveCodepoints(codepoint_list):
   return result
 
 
-def ParseOptions():
+def ParseArgs():
   """Parses command line options."""
-  parser = optparse.OptionParser()
-  parser.add_option('--jisx0201file', dest='jisx0201file',
-                    help='File path for the unicode\'s JIS0201.TXT file')
-  parser.add_option('--jisx0208file', dest='jisx0208file',
-                    help='File path for the unicode\'s JIS0208.TXT file')
-  parser.add_option('--output', dest='output',
-                    help='output file path. If not specified, '
-                    'output to stdout.')
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--jisx0201file',
+      type=argparse.FileType('r', encoding='utf-8'),
+      help="File path for the unicode's JIS0201.TXT file",
+  )
+  parser.add_argument(
+      '--jisx0208file',
+      type=argparse.FileType('r', encoding='utf-8'),
+      help="File path for the unicode's JIS0208.TXT file",
+  )
+  parser.add_argument(
+      '--output',
+      type=argparse.FileType('w', encoding='utf-8'),
+      default=sys.stdout,
+      help='output file path. If not specified, output to stdout.',
+  )
 
-  return parser.parse_args()[0]
+  return parser.parse_args()
 
 
 def GenerateJisX0208Bitmap(category_list, name):
-  r"""Generats bitmap data code.
+  r"""Generates bitmap data code.
 
   The generated data looks something like:
   namespace {
-  const uint64_t nameIndex = 0xXXXXXXXXXXXX;
-  const uint32_t name[NN][32] = {
+  constexpr uint64_t nameIndex = 0xXXXXXXXXXXXX;
+  constexpr uint32_t name[NN][32] = {
     {  // 0000 - 03FF
       0xXXXXXXXX, 0xXXXXXXXX, 0xXXXXXXXX, 0xXXXXXXXX,
       0xXXXXXXXX, 0xXXXXXXXX, 0xXXXXXXXX, 0xXXXXXXXX,
@@ -213,6 +219,9 @@ def GenerateJisX0208Bitmap(category_list, name):
   Args:
     category_list: a list of categories.
     name: a bitmap name.
+
+  Returns:
+    lines (list): Generated code
   """
   lines = []
 
@@ -221,8 +230,9 @@ def GenerateJisX0208Bitmap(category_list, name):
   # The result should be uint32_t, so group each consecutive
   # (at most) 32 code points.
   bit_list = []
-  for _, group in itertools.groupby(enumerate(category_list),
-                                    lambda args: args[0] // 32):  # codepoint/32
+  for _, group in itertools.groupby(
+      enumerate(category_list), lambda args: args[0] // 32
+  ):  # codepoint/32
     # Fill bits from LSB to MSB for each group.
     bits = 0
     for index, (_, category) in enumerate(group):
@@ -234,14 +244,16 @@ def GenerateJisX0208Bitmap(category_list, name):
   data = []
   data_size = 0
   # Group by 1024 characters (= 128 bit_list units).
-  for key, group1 in itertools.groupby(enumerate(bit_list),
-                                       lambda args: args[0] // (1024 // 32)):
+  for key, group1 in itertools.groupby(
+      enumerate(bit_list), lambda args: args[0] // (1024 // 32)
+  ):
     block = ['  {  // U+%04X - U+%04X\n' % (key * 1024, (key + 1) * 1024 - 1)]
     has_value = False
 
     # Output the content. Each line would have (at most) 4 integers
-    for _, group2 in itertools.groupby(group1,
-                                       lambda args: args[0] // 4):  # index/4
+    for _, group2 in itertools.groupby(
+        group1, lambda args: args[0] // 4
+    ):  # index/4
       line = []
       for _, bits in group2:
         line.append('0x%08X' % bits)
@@ -251,31 +263,34 @@ def GenerateJisX0208Bitmap(category_list, name):
     block.append('  },\n')
 
     if has_value:
-      index += (1 << key)
+      index += 1 << key
       data_size += 1
       data.extend(block)
 
-  lines.extend(['namespace {\n',
-                'const uint64_t %sIndex = 0x%016X;\n' % (name, index),
-                'const uint32_t %s[%d][32] = {\n' % (name, data_size)])
+  lines.extend([
+      'namespace {\n',
+      'constexpr uint64_t %sIndex = 0x%016X;\n' % (name, index),
+      'constexpr uint32_t %s[%d][32] = {\n' % (name, data_size),
+  ])
   lines.extend(data)
-  lines.extend(['};\n',
-                '}  // namespace\n'])
+  lines.extend(['};\n', '}  // namespace\n'])
 
   return lines
 
 
 def GenerateCharacterSetHeader(category_list):
-  """Generates lines of character_set.h file."""
+  """Generates lines of character_set.inc file."""
   bitmap_name = 'kJisX0208Bitmap'
   bitmap_size = 65536
 
   lines = []
 
   # File header comments.
-  lines.extend(['// This file is generated by base/gen_character_set.py\n',
-                '// Do not edit me!\n',
-                '\n'])
+  lines.extend([
+      '// This file is generated by base/gen_character_set.py\n',
+      '// Do not edit me!\n',
+      '\n',
+  ])
 
   # We use bitmap to check JISX0208 for code point 0 to 65535.
   lines.extend(GenerateJisX0208Bitmap(category_list[:bitmap_size], bitmap_name))
@@ -285,24 +300,18 @@ def GenerateCharacterSetHeader(category_list):
 
 
 def main():
-  options = ParseOptions()
+  args = ParseArgs()
 
   # Generates lines of the header file.
-  categorizer = CodePointCategorizer(options.jisx0201file, options.jisx0208file)
+  categorizer = CodePointCategorizer(args.jisx0201file, args.jisx0208file)
   category_list = [
       categorizer.GetCategory(codepoint)
-      for codepoint in range(categorizer.MaxCodePoint() + 1)]
+      for codepoint in range(categorizer.MaxCodePoint() + 1)
+  ]
   generated_character_set_header = GenerateCharacterSetHeader(category_list)
 
   # Write the result.
-  if options.output:
-    output = open(options.output, 'w')
-    try:
-      output.writelines(generated_character_set_header)
-    finally:
-      output.close()
-  else:
-    sys.stdout.writelines(generated_character_set_header)
+  args.output.writelines(generated_character_set_header)
 
 
 if __name__ == '__main__':
