dc2db9b62d470bd8d38b5e136e3f0e4880256a32
Emma Haruka Iwao <yuryu@google.com>
2023-09-07 05:19:28 +0000

    Cleanup: use = default for empty constructors, delete empty destructors
    #codehealth
    
    PiperOrigin-RevId: 563312626

--- src/rewriter/remove_redundant_candidate_rewriter.cc
+++ src/rewriter/remove_redundant_candidate_rewriter.cc
@@ -32,12 +32,9 @@
 #include "converter/segments.h"
 #include "protocol/commands.pb.h"
 #include "request/conversion_request.h"
+#include "rewriter/rewriter_interface.h"
 
 namespace mozc {
-
-RemoveRedundantCandidateRewriter::RemoveRedundantCandidateRewriter() {}
-
-RemoveRedundantCandidateRewriter::~RemoveRedundantCandidateRewriter() {}
 
 int RemoveRedundantCandidateRewriter::capability(
     const ConversionRequest &request) const {
