9b5e4edae43b90a7575d0575697f7241a2f002a1
Yohei Yukawa <yukawa@google.com>
2023-10-25 20:23:34 +0000

    Fix mozc_tool build failure on Windows
    This is a follow up to 489f7533473a32f14b7524f4b8edaf4b4a576630.
    
    #codehealth
    
    PiperOrigin-RevId: 576626249

--- src/gui/base/msime_user_dictionary_importer.cc
+++ src/gui/base/msime_user_dictionary_importer.cc
@@ -134,9 +134,9 @@ class MSIMEImportIterator
 
     std::string name;
     for (int i = 0; i < pos_size; ++i) {
-      EncodingUtil::SjisToUtf8(reinterpret_cast<char *>(pos_table->szName),
-                               &name);
-      pos_map_.insert(std::make_pair(pos_table->nPos, name));
+      pos_map_.try_emplace(pos_table->nPos,
+                           EncodingUtil::SjisToUtf8(
+                               reinterpret_cast<char *>(pos_table->szName)));
       ++pos_table;
     }
 
@@ -195,10 +195,8 @@ class MSIMEImportIterator
       // set comment
       if (buf_[index_].pvComment != nullptr) {
         if (buf_[index_].uct == IFED_UCT_STRING_SJIS) {
-          EncodingUtil::SjisToUtf8(
-              reinterpret_cast<const char *>(buf_[index_].pvComment),
-              &entry->comment);
-        } else if (buf_[index_].uct == IFED_UCT_STRING_UNICODE) {
+          entry->comment = EncodingUtil::SjisToUtf8(
+              reinterpret_cast<const char *>(buf_[index_].pvComment));        } else if (buf_[index_].uct == IFED_UCT_STRING_UNICODE) {
           Util::WideToUtf8(
               reinterpret_cast<const wchar_t *>(buf_[index_].pvComment),
               &entry->comment);
