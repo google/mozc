78aee2ace976c248e6929bd00d25bfb4227ad487
Yohei Yukawa <yukawa@google.com>
2024-02-24 04:56:07 +0000

    Fix #877: Switch to Qt 6.6.2 for macOS and Windows
    With this commit macOS and Windows builds switch to Qt 6.6.2.
    
    Here is the release note:
    https://code.qt.io/cgit/qt/qtreleasenotes.git/about/qt/6.6.2/release-note.md
    
    PiperOrigin-RevId: 609914103

--- src/build_tools/update_deps.py
+++ src/build_tools/update_deps.py
@@ -81,9 +81,9 @@ class ArchiveInfo:
 
 
 QT6 = ArchiveInfo(
-    url='https://download.qt.io/archive/qt/6.6/6.6.1/submodules/qtbase-everywhere-src-6.6.1.tar.xz',
-    size=48370760,
-    sha256='450c5b4677b2fe40ed07954d7f0f40690068e80a94c9df86c2c905ccd59d02f7',
+    url='https://download.qt.io/archive/qt/6.6/6.6.2/submodules/qtbase-everywhere-src-6.6.2.tar.xz',
+    size=48689304,
+    sha256='b89b426b9852a17d3e96230ab0871346574d635c7914480a2a27f98ff942677b',
 )
 
 WIX = ArchiveInfo(
