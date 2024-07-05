8d3a2970014f03cdddf7291b71d632999563f0b6
Hiroyuki Komatsu <komatsu@google.com>
2024-07-05 12:15:45 +0000

    Migrate entries of dictionary_oss/aux_dictionary.tsv to dictionary_manual/words.tsv
    * words.tsv uses the middle cost of the same POS words.
    * If the middle cost works well, no base entries are required.
    * It simplifies the process of vocabulary management.
    
    #typequality
    #codehealth
    
    PiperOrigin-RevId: 649623860

--- src/dictionary/gen_aux_dictionary.py
+++ src/dictionary/gen_aux_dictionary.py
@@ -45,12 +45,96 @@ import argparse
 import sys
 
 
+class Entry():
+  """Class for an entry in the dictionary."""
+
+  def __init__(self, key: str, value: str, lid: str, rid: str, cost: int):
+    self.key = key
+    self.value = value
+    self.lid = lid
+    self.rid = rid
+    self.cost = cost
+
+  def EntryKey(self) -> str:
+    return '\t'.join([self.lid, self.rid, self.key, self.value])
+
+  @classmethod
+  def Parse(cls, line: str) -> 'Entry':
+    key, lid, rid, cost, value, *_ = line.rstrip().split('\t')
+    return Entry(key, value, lid, rid, int(cost))
+
+
+class EntryList():
+  """Class for a list of entries."""
+
+  def __init__(self):
+    self.entries = []
+    self.sorted = False
+
+  def Add(self, entry: Entry) -> None:
+    self.entries.append(entry)
+    self.sorted = False
+
+  def Sort(self) -> None:
+    if self.sorted:
+      return
+    self.entries.sort(key=lambda entry: entry.cost)
+    self.sorted = True
+
+  def AtRatio(self, ratio: float) -> Entry:
+    self.Sort()
+    ratio = max(0, min(ratio, 1.0))
+    index = int(max(len(self.entries) - 1, 0) * ratio)
+    return self.entries[index]
+
+
+class PosIds():
+  """Class for pos ids."""
+
+  def __init__(self):
+    self.ids = {}  # "pos_name" -> "pos_id"
+    # Keys are subset of data/rules/user_pos.def
+    self.pos_alias = {
+        '名詞': '名詞,一般,*,*,*,*,*',
+        '固有名詞': '名詞,固有名詞,一般,*,*,*,*',
+        '人名': '名詞,固有名詞,人名,一般,*,*,*',
+        '姓': '名詞,固有名詞,人名,姓,*,*,*',
+        '名': '名詞,固有名詞,人名,名,*,*,*',
+        '組織': '名詞,固有名詞,組織,*,*,*,*',
+        '地名': '名詞,固有名詞,地域,一般,*,*,*',
+        '名詞サ変': '名詞,サ変接続,*,*,*,*,*',
+        '名詞形動': '名詞,形容動詞語幹,*,*,*,*,*',
+        '副詞': '副詞,一般,*,*,*,*,*',
+        '連体詞': '連体詞,*,*,*,*,*,*',
+        '接続詞': '接続詞,*,*,*,*,*,*',
+        '感動詞': '感動詞,*,*,*,*,*,*',
+        '接頭語': '接頭詞,名詞接続,*,*,*,*,*',
+        '助数詞': '名詞,接尾,助数詞,*,*,*,*',
+        '接尾一般': '名詞,接尾,一般,*,*,*,*',
+        '接尾人名': '名詞,接尾,人名,*,*,*,*',
+        '接尾地名': '名詞,接尾,地域,*,*,*,*',
+    }
+
+  def ParseFile(self, id_def: str):
+    for line in open(id_def, encoding='utf-8'):
+      pos_id, name = line.rstrip().split(' ')
+      self.ids[name] = pos_id
+
+    for alias, name in self.pos_alias.items():
+      pos_id = self.ids[name]
+      self.ids[alias] = pos_id
+
+  def GetId(self, pos_name: str) -> str:
+    return self.ids.get(pos_name, '')
+
+
 class Dictionary():
   """Class for dictionary0*.txt."""
 
   def __init__(self):
     self.entry_set = set()
-    self.data = {}
+    self.data = {}  # "<key>\t<value>" -> [entry1, entry2, ...]
+    self.pos_entry_map = {}  # "<lid>\t<rid>" -> EntryList
 
   def Parse(self, dictionary_txts):
     for file in dictionary_txts:
@@ -59,18 +143,20 @@ class Dictionary():
   def _ParseFile(self, file):
     """Parses the file and set values to data and entry_set."""
     for line in open(file, encoding='utf-8'):
-      key, lid, rid, cost, value, *_ = line.rstrip().split('\t')
+      entry = Entry.Parse(line)
 
       # Update data
-      data_key = '\t'.join([key, value])
-      data_value = [lid, rid, cost]
-      self.data.setdefault(data_key, []).append(data_value)
-
-      cost = int(cost)
+      data_key = '\t'.join([entry.key, entry.value])
+      self.data.setdefault(data_key, []).append(entry)
 
       # Update entry_set
-      entry_key = '\t'.join([lid, rid, key, value])
-      self.entry_set.add(entry_key)
+      self.entry_set.add(entry.EntryKey())
+
+      # Update pos_map
+      pos_key = '\t'.join([entry.lid, entry.rid])
+      if pos_key not in self.pos_entry_map:
+        self.pos_entry_map[pos_key] = EntryList()
+      self.pos_entry_map[pos_key].Add(entry)
 
   def Exists(self, key, value, lid, rid):
     entry_key = '\t'.join([lid, rid, key, value])
@@ -80,6 +166,10 @@ class Dictionary():
     data_key = '\t'.join([key, value])
     return self.data.get(data_key)
 
+  def GetCost(self, pos_id: str, ratio: float) -> int:
+    pos_key = '\t'.join([pos_id, pos_id])
+    return self.pos_entry_map[pos_key].AtRatio(ratio).cost
+
 
 class AuxDictionary():
   """Class for aux_dictionary.tsv."""
@@ -87,6 +177,7 @@ class AuxDictionary():
   def __init__(self, dictionary):
     self.dictionary = dictionary
     self.aux_list = []
+    self.aux_set = set()
 
   def Parse(self, aux_tsv, strict=False):
     """Parses the file and update aux_list."""
@@ -104,12 +195,17 @@ class AuxDictionary():
           print('Warning: ' + message, file=sys.stderr)
           continue
 
-      for base_lid, base_rid, base_cost in data_list:
-        if self.dictionary.Exists(key, value, base_lid, base_rid):
+      for entry in data_list:
+        if self.dictionary.Exists(key, value, entry.lid, entry.rid):
           # Not overwrite the existing entry.
           continue
-        cost = int(base_cost) + int(cost_offset)
-        self.aux_list.append([key, base_lid, base_rid, str(cost), value])
+        cost = int(entry.cost) + int(cost_offset)
+        self.aux_list.append([key, entry.lid, entry.rid, str(cost), value])
+        self.aux_set.add('\t'.join([entry.lid, entry.rid, key, value]))
+
+  def Exists(self, key, value, lid, rid):
+    entry_key = '\t'.join([lid, rid, key, value])
+    return entry_key in self.aux_set
 
   def WriteFile(self, output):
     with open(output, 'w', encoding='utf-8') as file:
@@ -117,10 +213,54 @@ class AuxDictionary():
         file.write('\t'.join(aux_entry) + '\n')
 
 
+class WordsDictionary():
+  """Class for aux_dictionary.tsv."""
+
+  def __init__(self, dictionary, aux):
+    self.dictionary = dictionary
+    self.aux = aux
+    self.words = []
+
+  def Parse(self, words_tsv, id_def, ratio=0.5):
+    """Parses the file and update aux_list."""
+    pos_ids = PosIds()
+    pos_ids.ParseFile(id_def)
+
+    for line in open(words_tsv, encoding='utf-8'):
+      if line.startswith('#') or not line.rstrip():
+        continue
+
+      key, value, pos = line.rstrip().split('\t')
+      pos_id = pos_ids.GetId(pos)
+      if not pos_id:
+        print('Error: ' + pos + ' is an invalid pos', file=sys.stderr)
+        print(line, file=sys.stderr)
+        sys.exit(1)
+
+      # Do not overwrite the existing entry.
+      if self.dictionary.Exists(key, value, pos_id, pos_id) or self.aux.Exists(
+          key, value, pos_id, pos_id
+      ):
+        continue
+
+      # Use the same cost as the entry of pos_id and ratio.
+      # ratio indicates the position in the words sorted by cost.
+      # 0.5 means the middle of the list. 0.0 means the top of the list.
+      cost = self.dictionary.GetCost(pos_id, ratio)
+      self.words.append([key, pos_id, pos_id, str(cost), value])
+
+  def WriteFile(self, output):
+    with open(output, 'a', encoding='utf-8') as file:
+      for word in self.words:
+        file.write('\t'.join(word) + '\n')
+
+
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--aux_tsv')
+  parser.add_argument('--words_tsv')
   parser.add_argument('--dictionary_txts', nargs='+')
+  parser.add_argument('--id_def')
   parser.add_argument('--output')
   parser.add_argument('--strict', action='store_true')
   args = parser.parse_args()
@@ -132,6 +272,10 @@ def main():
   aux.Parse(args.aux_tsv, strict=args.strict)
   aux.WriteFile(args.output)
 
+  if args.words_tsv and args.id_def:
+    words = WordsDictionary(dictionary, aux)
+    words.Parse(args.words_tsv, args.id_def)
+    words.WriteFile(args.output)
 
 if __name__ == '__main__':
   main()
