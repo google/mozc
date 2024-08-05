c067465fc25f231c5d786902831d1fc5ef36e31a
Hiroyuki Komatsu <komatsu@google.com>
2024-08-05 07:14:37 +0000

    Update build_diskimage_mac.py to explicitly take input/output files.
    * used argparse instead of optparse.
    
    #codehealth
    
    PiperOrigin-RevId: 659446462

--- src/build_tools/build_diskimage_mac.py
+++ src/build_tools/build_diskimage_mac.py
@@ -28,19 +28,16 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-r"""Create a dmg file.
+"""Create a dmg file.
 
-  % python build_diskimage_mac.py --build_dir=/path/to/build_dir \
-    --volname=GoogleJapaneseInput GoogleJapaneseInput.mpkg ... \
+  % python build_diskimage_mac.py --build_dir=/path/to/build_dir
+    --volname=GoogleJapaneseInput GoogleJapaneseInput.mpkg ...
 
   This file creates the dmg file with the volname, whose contents are
   the remaining args.
 """
 
-__author__ = "mukai"
-
-import logging
-import optparse
+import argparse
 import os
 from os import path
 import shutil
@@ -69,46 +66,55 @@ def ParseOptions():
   Returns:
     A pair of (options data, remaining args)
   """
-  parser = optparse.OptionParser()
-  parser.add_option("--build_dir", dest="build_dir")
-  parser.add_option("--volname", dest="volname", default="GoogleJapaneseInput")
-  parser.add_option("--pkgfile", dest="pkgfile")
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--volname", default="GoogleJapaneseInput")
+  parser.add_argument("--output", required=True)
+  parser.add_argument("--pkg_file", required=True)
+  parser.add_argument("--keystone_install_file")
 
   return parser.parse_args()
 
 
 def main():
   """The main function."""
-  (options, args) = ParseOptions()
-  if options.build_dir is None:
-    logging.error("--build_dir is not specified.")
-    exit(-1)
-
-  build_dir = options.build_dir
+  options = ParseOptions()
 
   # setup volume directory
-  temp_dir = tempfile.mkdtemp()
-  CopyFile(path.join(build_dir, ".keystone_install"), temp_dir)
-  os.chmod(path.join(temp_dir, ".keystone_install"), 0o755)  # rwxr-xr-x
-  for a in args:
-    CopyFile(path.join(build_dir, a), temp_dir)
+  temp_root_dir = tempfile.mkdtemp()
+  temp_dir = path.join(temp_root_dir, "dmg")
+  os.mkdir(temp_dir)
+
+  # Copy .keystone_install to the temp directory and make it executable.
+  if options.keystone_install_file:
+    keystone_install_file = path.join(temp_dir, ".keystone_install")
+    CopyFile(options.keystone_install_file, keystone_install_file)
+    os.chmod(keystone_install_file, 0o755)  # rwxr-xr-x
+
+  # Copy .pkg file to the temp directory.
+  CopyFile(options.pkg_file, temp_dir)
 
   # build a disk image
-  dmg_path = path.join(build_dir, options.volname + ".dmg")
+  dmg_path = path.join(temp_root_dir, path.basename(options.output))
   # hdiutil create will fail if there is already a target dmg file.
   if path.exists(dmg_path):
     os.remove(dmg_path)
-  process = subprocess.Popen(["/usr/bin/hdiutil", "create",
-                              "-srcfolder", temp_dir,
-                              "-volname", options.volname,
-                              # "SPUD" and "HFS+"" are required for KeyStone.
-                              # "GPTSPUD" and "APFS" are default values.
-                              "-layout", "SPUD", "-fs", "HFS+",
-                              dmg_path])
-  process.wait()
+  subprocess.run(
+      [
+          "/usr/bin/hdiutil", "create",
+          "-srcfolder", temp_dir,
+          "-volname", options.volname,
+          # "SPUD" and "HFS+" are required for KeyStone.
+          # "GPTSPUD" and "APFS" are default values.
+          "-layout", "SPUD",
+          "-fs", "HFS+",
+          dmg_path,
+      ],
+      check=True,
+  )
+  CopyFile(dmg_path, options.output)
 
   # clean up the temp directory
-  shutil.rmtree(temp_dir)
+  shutil.rmtree(temp_root_dir)
 
 
 if __name__ == "__main__":
