43c165d26f689f875397b7fd101e48df475309b8
Hiroyuki Komatsu <komatsu@google.com>
2024-09-17 06:58:33 +0000

    Update GoogleJapaneseInputServer and remove getServer.
    #codehealth
    
    PiperOrigin-RevId: 675440558

--- src/mac/GoogleJapaneseInputServer.mm
+++ src/mac/GoogleJapaneseInputServer.mm
@@ -31,36 +31,15 @@
 
 #include <string>
 
-#include "absl/base/call_once.h"
 #include "absl/log/log.h"
 #include "base/const.h"
 #include "protocol/commands.pb.h"
-
-GoogleJapaneseInputServer *g_imkServer = nil;
-
-namespace {
-void InitializeServer() {
-  NSBundle *bundle = [NSBundle mainBundle];
-  NSDictionary *infoDictionary = [bundle infoDictionary];
-  NSString *connectionName = [infoDictionary objectForKey:@"InputMethodConnectionName"];
-  if (connectionName == nil || ![connectionName isKindOfClass:[NSString class]]) {
-    LOG(ERROR) << "InputMethodConnectionName is not found or incorrect. "
-               << "Possibly Info.plist is broken.";
-    return;
-  }
-
-  g_imkServer = [[GoogleJapaneseInputServer alloc] initWithName:connectionName
-                                               bundleIdentifier:[bundle bundleIdentifier]];
-  [g_imkServer registerRendererConnection];
-}
-absl::once_flag gOnceForServer;
-}  // namespace
 
 @implementation GoogleJapaneseInputServer
 - (BOOL)registerRendererConnection {
   NSString *connectionName = @kProductPrefix "_Renderer_Connection";
   renderer_conection_ = [[NSConnection alloc] init];
-  [renderer_conection_ setRootObject:g_imkServer];
+  [renderer_conection_ setRootObject:self];
   return [renderer_conection_ registerName:connectionName];
 }
 
@@ -70,7 +49,8 @@ absl::once_flag gOnceForServer;
   }
 
   mozc::commands::SessionCommand command;
-  if (!command.ParseFromArray([data bytes], [data length])) {
+  int32_t length = static_cast<int32_t>([data length]);
+  if (!command.ParseFromArray([data bytes], length)) {
     return;
   }
 
@@ -79,7 +59,8 @@ absl::once_flag gOnceForServer;
 
 - (void)outputResult:(NSData *)data {
   mozc::commands::Output output;
-  if (!output.ParseFromArray([data bytes], [data length])) {
+  int32_t length = static_cast<int32_t>([data length]);
+  if (!output.ParseFromArray([data bytes], length)) {
     return;
   }
 
@@ -88,10 +69,5 @@ absl::once_flag gOnceForServer;
 
 - (void)setCurrentController:(id<ControllerCallback>)controller {
   current_controller_ = controller;
-}
-
-+ (GoogleJapaneseInputServer *)getServer {
-  absl::call_once(gOnceForServer, &InitializeServer);
-  return g_imkServer;
 }
 @end
--- src/mac/main.mm
+++ src/mac/main.mm
@@ -27,9 +27,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#import <Carbon/Carbon.h>
 #import <Cocoa/Cocoa.h>
-#import <InputMethodKit/IMKServer.h>
 
 #include <memory>
 
@@ -57,12 +55,20 @@ int main(int argc, char *argv[]) {
 #endif  // GOOGLE_JAPANESE_INPUT_BUILD
   mozc::InitMozc(argv[0], &argc, &argv);
 
-  IMKServer *imkServer = [GoogleJapaneseInputServer getServer];
+  // Initialize imkServer
+  NSBundle *bundle = [NSBundle mainBundle];
+  NSDictionary *infoDictionary = [bundle infoDictionary];
+  NSString *connectionName = [infoDictionary objectForKey:@"InputMethodConnectionName"];
+  GoogleJapaneseInputServer *imkServer =
+    [[GoogleJapaneseInputServer alloc] initWithName:connectionName
+                                   bundleIdentifier:[bundle bundleIdentifier]];
+
   if (!imkServer) {
     LOG(FATAL) << mozc::kProductNameInEnglish << " failed to initialize";
     return -1;
   }
   DLOG(INFO) << mozc::kProductNameInEnglish << " initialized";
+  [imkServer registerRendererConnection];
 
   [GoogleJapaneseInputController initializeConstants];
 
