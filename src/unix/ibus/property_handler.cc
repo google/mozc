dfd3c728f6779e3f4104bf284276ad09d022397b
Pavel <39219491+stinger222@users.noreply.github.com>
2024-09-23 19:45:16 +0300

    Update property_handler.cc

--- src/unix/ibus/property_handler.cc
+++ src/unix/ibus/property_handler.cc
@@ -2,7 +2,7 @@
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
+// modification, are permitted provided that  the following conditions are
 // met:
 //
 //     * Redistributions of source code must retain the above copyright
