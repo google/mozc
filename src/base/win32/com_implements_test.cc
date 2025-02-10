0a143847a661b7c28c8268722f4d8de8b56c3bd8
Hiroyuki Komatsu <komatsu@google.com>
2025-02-10 08:54:41 +0000

    Fix build failures for Windows.
    * Fixed the unused error in process_watch_dog_test.cc
    * Fixed the non-virtual destructor error in com_inplements_test.cc
    
    #codehealth
    
    PiperOrigin-RevId: 725105920

--- src/base/win32/com_implements_test.cc
+++ src/base/win32/com_implements_test.cc
@@ -44,19 +44,31 @@ namespace {
 
 // Mock interfaces for testing.
 MIDL_INTERFACE("A03A80F4-9254-4C8B-AF25-0674FCED18E5")
-IMock1 : public IUnknown { STDMETHOD(Test1)() = 0; };
+IMock1 : public IUnknown {
+  virtual ~IMock1() = default;
+  STDMETHOD(Test1)() = 0;
+};
 
 MIDL_INTERFACE("863EF391-8485-4257-8423-8D919D1AE8DC")
-IMock2 : public IUnknown { STDMETHOD(Test2)() = 0; };
+IMock2 : public IUnknown {
+  virtual ~IMock2() = default;
+  STDMETHOD(Test2)() = 0;
+};
 
 MIDL_INTERFACE("7CC0C082-8CA5-4A87-97C4-4FC14FBCE0B3")
-IDerived : public IMock1 { STDMETHOD(Derived()) = 0; };
+IDerived : public IMock1 {
+  virtual ~IDerived() = default;
+  STDMETHOD(Derived()) = 0;
+};
 
 MIDL_INTERFACE("F2B8DCC5-226C-4123-8F78-2BC36B574629")
-IDerivedDerived : public IDerived{};
+IDerivedDerived : public IDerived { virtual ~IDerivedDerived() = default; };
 
 MIDL_INTERFACE("9C1A7121-BF54-4826-856E-55A90864EE64")
-IRefCount : public IUnknown { STDMETHOD_(ULONG, RefCount)() = 0; };
+IRefCount : public IUnknown {
+  virtual ~IRefCount() = default;
+  STDMETHOD_(ULONG, RefCount)() = 0;
+};
 
 }  // namespace
 
