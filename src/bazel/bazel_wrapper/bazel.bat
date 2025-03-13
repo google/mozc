5780bf9aaa50555bd80a16b07a5bdf0af1a9fa83
Yohei Yukawa <yukawa@chromium.org>
2025-03-14 02:14:04 +0900

    Start using `clang-cl` for Windows Bazel build (#1198)
    With this commit we start using 'clang-cl' to build Mozc for Windows
    with Bazel.
    
    While the original motivation of switching to 'clang-cl' is that
    protobuf is planning to stop supporting cl.exe with bazel build in
    protobuf v34 [1], there is also an on-going build breakage that happens
    when combining protobuf and abseil-cpp 20250127.0, which is not yet
    fixed [2]. The latter one is more critical for us because more and more
    bzlmod dependencies start requiring abseil-cpp 20250127.0 or higher.
    
    There must be no impact on Windows GYP build.
    
    Closes #1179.
    
     [1]: https://github.com/protocolbuffers/protobuf/issues/20085
     [2]: https://github.com/protocolbuffers/protobuf/issues/20331
    
    PiperOrigin-RevId: 736417401

--- src/bazel/bazel_wrapper/bazel.bat
+++ src/bazel/bazel_wrapper/bazel.bat
@@ -1,4 +1,16 @@
 @echo off
+
+rem set BAZEL_LLVM only if clang-cl exists under third_party/llvm.
+set TMP_THIRD_PARTY_LLVN_DIR=third_party\llvm\clang+llvm-19.1.7-x86_64-pc-windows-msvc
+set TMP_MOZC_BAZEL_WRAPPER_DIR=%~dp0
+set TMP_MOZC_SRC_DIR=%TMP_MOZC_BAZEL_WRAPPER_DIR:~0,-21%
+set TMP_MOZC_LLVM_DIR=%TMP_MOZC_SRC_DIR%\%TMP_THIRD_PARTY_LLVN_DIR%
+if exist %TMP_MOZC_LLVM_DIR% set BAZEL_LLVM=%TMP_MOZC_LLVM_DIR%
+set TMP_MOZC_BAZEL_WRAPPER_DIR=
+set TMP_MOZC_SRC_DIR=
+set TMP_MOZC_LLVM_DIR=
+set TMP_THIRD_PARTY_LLVN_DIR=
+
 %BAZEL_REAL% %* & call:myexit
 
 :myexit
