3824a2d95b30f46fd16831665082e5113f1432b2
Yohei Yukawa <yukawa@chromium.org>
2025-04-06 00:41:58 +0900

    Update `clang-cl` to 20.1.1 for Windows Bazel build (#1242)
    LLVM 20.1.1 was released on March 19, 2025.
    
     * https://github.com/llvm/llvm-project/releases/tag/llvmorg-20.1.1
    
    Let's update our Windows Bazel build to use clang-cl 20.1.1 there.
    
    No user-visible behavior change is intended in the final artifacts.
    
    PiperOrigin-RevId: 743946547

--- src/bazel/bazel_wrapper/bazel.bat
+++ src/bazel/bazel_wrapper/bazel.bat
@@ -4,7 +4,7 @@ set TMP_MOZC_BAZEL_WRAPPER_DIR=%~dp0
 set TMP_MOZC_SRC_DIR=%TMP_MOZC_BAZEL_WRAPPER_DIR:~0,-21%
 
 rem set BAZEL_LLVM only if clang-cl exists under third_party/llvm.
-set TMP_MOZC_LLVM_DIR=%TMP_MOZC_SRC_DIR%\third_party\llvm\clang+llvm-20.1.0-x86_64-pc-windows-msvc
+set TMP_MOZC_LLVM_DIR=%TMP_MOZC_SRC_DIR%\third_party\llvm\clang+llvm-20.1.1-x86_64-pc-windows-msvc
 if exist %TMP_MOZC_LLVM_DIR% set BAZEL_LLVM=%TMP_MOZC_LLVM_DIR%
 set TMP_MOZC_LLVM_DIR=
 
