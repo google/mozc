7f027861073102491ae8c500e314c5443a79a5c6
Yohei Yukawa <yukawa@ciceroaware.dev>
2026-04-27 19:44:47 +0900

    Remove `Singleton` from `ModeSwitchingHandler`
    As part of our ongoing efforts to remove the dependency on Singleton,
    this commit rewrites composer::ModeSwitchingHandler without the
    Singleton class dependency.
    
    This is purely mechanical refactoring, and there must be no observable
    behavioral change in production.

--- src/composer/mode_switching_handler.h
+++ src/composer/mode_switching_handler.h
@@ -33,9 +33,6 @@
 #ifndef MOZC_COMPOSER_MODE_SWITCHING_HANDLER_H_
 #define MOZC_COMPOSER_MODE_SWITCHING_HANDLER_H_
 
-#include <string>
-
-#include "absl/container/flat_hash_map.h"
 #include "absl/strings/string_view.h"
 
 namespace mozc {
@@ -43,8 +40,8 @@ namespace composer {
 
 class ModeSwitchingHandler {
  public:
-  ModeSwitchingHandler();
-  ~ModeSwitchingHandler() = default;
+  ModeSwitchingHandler() = delete;
+  ~ModeSwitchingHandler() = delete;
 
   enum ModeSwitching {
     NO_CHANGE,
@@ -65,21 +62,12 @@ class ModeSwitchingHandler {
   // Returns a Rule for the current preedit. |key| is the string which the user
   // actually typed. display_mode and input_mode are stored rules controlling
   // the composer. Returns NO_CHANGE if the key doesn't match the stored rules.
-  Rule GetModeSwitchingRule(absl::string_view key) const;
-
-  // Gets the singleton instance of this class.
-  static ModeSwitchingHandler* GetModeSwitchingHandler();
+  static Rule GetModeSwitchingRule(absl::string_view key);
 
   // Matcher to Windows drive letters like "C:\".
   // TODO(team): This static method is internal use only.  It's public for
   // testing purpose.
   static bool IsDriveLetter(absl::string_view key);
-
- private:
-  // Adds a rule for mode switching. The rule is passed by value as it's small.
-  void AddRule(absl::string_view key, Rule rule);
-
-  absl::flat_hash_map<std::string, Rule> patterns_;
 };
 
 }  // namespace composer
